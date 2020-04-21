d07c5ca4e2e90cff0b57a00ff2006021451a65c9
Abhi10699 <patelabhi10699@gmail.com>
2020-04-22 04:00:25 +0530

    main page

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require "./pages/_includes/header.php"?>
+<?php require "./pages/_includes/body.php"?>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-3">Eclass</h1>
+    <p class="lead">Future of online learning..</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="pages/auth/signin.php" role="button">Get Started</a>
+    </p>
+  </div>
+</div>
+<?php require "./pages/_includes/footer.php"?>
