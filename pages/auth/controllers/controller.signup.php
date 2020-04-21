f0ed87224d127cd5e9f81a8413e796e3d8f9f273
Abhi10699 <patelabhi10699@gmail.com>
2020-04-22 04:03:00 +0530

    registeration controller

---
+++ pages/auth/controllers/controller.signup.php
@@ -0,0 +1,109 @@
+<?php
+
+// load User model
+require ($_SERVER["DOCUMENT_ROOT"] . "/models/entities/User.php");
+
+// error handler messages
+$error_username = "";
+$error_email = "";
+$error_password = "";
+$error_confirm = "";
+$error_general = "";
+// success handler messages
+
+$success_message = "";
+
+
+// handle form submit
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  $formResponse = validate();
+  // check if form is valid
+
+  if($formResponse["valid"] == true){
+    // create user;
+   createUser($formResponse);
+  }
+}
+
+// create user function
+
+function createUser(array $userData) : void{
+
+  $user = User::NewUser($userData["username"],$userData["email"],$userData["password"]);
+  $userAdded = $user->saveUser();
+  
+  if($userAdded){
+    echo true;
+  }
+  else{
+    echo false;
+  }
+}
+
+// form validation function  
+// TODO: Add constraints to inputs
+
+function validate(): array
+{
+  // Error messages
+  global $error_username, $error_confirm, $error_email, $error_password;
+
+  // validation status
+  $valid = false;
+  $username = $_POST["username"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $confirmPassword = $_POST["confirmPassword"];
+
+  // check username
+
+  if (empty($username)) {
+    $error_username = "Username is required.";
+    $valid = false;
+  } else {
+    $valid = true;
+  }
+
+  // check email
+
+  if (empty($email)) {
+    $error_email = "Email is required.";
+    $valid = false;
+  } else {
+    $valid = true;
+  }
+
+  // check password
+
+  if (empty($password)) {
+    $error_password = "Password is required.";
+    $valid = false;
+  } else {
+    $valid = true;
+  }
+
+  // check confirm password
+
+  if (empty($confirmPassword) && ($password == $confirmPassword)) {
+    $error_confirm = "Passwords donot match.";
+    $valid = false;
+  } else {
+    $valid = true;
+  }
+
+
+  if ($valid == false) {
+    return array(
+      "valid" => $valid,
+    );
+  } else {
+    return array(
+      "valid" => $valid,
+      "username" => $username,
+      "password" => $password,
+      "email" => $email
+    );
+  }
+}
