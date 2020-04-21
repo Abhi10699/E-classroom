b00e57bd83bb8e18d8ce4638038f6e1397f53773
Abhi10699 <patelabhi10699@gmail.com>
2020-04-22 04:04:01 +0530

    dashboard page

---
+++ pages/dashboard/index.php
@@ -0,0 +1,15 @@
+<!-- Load includes -->
+<?php require("../_includes/header.php")?>
+
+<link rel="stylesheet" href="style.css">
+
+<?php require("../_includes/body.php"); ?>
+
+
+<!-- Load controller -->
+
+<?php require("./controllers/controller.index.php")?>
+<div class="container">
+  
+</div>
+<?php require("../_includes/footer.php")?>
