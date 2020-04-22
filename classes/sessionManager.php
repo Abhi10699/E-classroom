06cb7bed2f841c625074db083a0f7e6516098656
Abhi10699 <patelabhi10699@gmail.com>
2020-04-23 02:12:32 +0530

    session manager file

---
+++ classes/sessionManager.php
@@ -0,0 +1,12 @@
+<?php
+  class SessionManager{
+    // Session members
+    private $sessionMembers = array(
+      "authenticated" => false,
+    );
+
+    public static function updateSession(string $sessName,$value) : void{
+      
+    }
+  }
+?>
