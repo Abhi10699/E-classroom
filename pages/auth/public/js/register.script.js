f5bbf75c0d2425da9537f33754f0a160f104abce
Abhi10699 <patelabhi10699@gmail.com>
2020-04-23 02:10:01 +0530

    minor changes to process json response from server

--- pages/auth/public/js/register.script.js
+++ pages/auth/public/js/register.script.js
@@ -1,34 +1,28 @@
-async function register(){
- const username = document.getElementById("username").value;
- const email = document.getElementById("email").value;
- const password = document.getElementById("password").value;
- const confirmPass = document.getElementById("confirmPass").value;
- const form = document.getElementById("form-register");
- const formData = new FormData(form);
+async function register() {
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPass = document.getElementById("confirmPass").value;
+  const form = document.getElementById("form-register");
+  const formData = new FormData(form);
 
- formData.append("username",username);
- formData.append("email",email);
- formData.append("password",password);
- formData.append("confirmPassword",confirmPass);
+  formData.append("username", username);
+  formData.append("email", email);
+  formData.append("password", password);
+  formData.append("confirmPassword", confirmPass);
 
+  const response = await fetch("./controllers/controller.signup.php", {
+    method: "post",
+    body: formData,
+  });
 
- const response = await fetch("./controllers/controller.signup.php",{
-   method:"post",
-   body:formData
- })
+  const result = await response.json();
 
- const result = await response.text();
- const data = await result;
+  // redirect to dashboard if user is created successfully
 
- console.log(data);
-
- if(data == 1){
-   // registered successfully
-   console.log("Success");
- }
- else{
-   // something went wrong bro.
-   console.log("failed");
-
- }
+  if (result.userCreated) {
+    window.location = "/pages/dashboard/";
+  } else {
+    alert("Something went wrong");
+  }
 }
