2c6de783adbea7493d49eb071747aa4dc9231b1e
Abhi10699 <patelabhi10699@gmail.com>
2020-04-24 04:16:52 +0530

    added a log out btm

--- pages/_includes/body.php
+++ pages/_includes/body.php
@@ -1,6 +1,18 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-sm navbar-light bg-light">
-    <a class="navbar-brand" href="../../index.php">Google Classroom</a>
+  <nav class="navbar navbar-expand navbar-light bg-light">
+    <a class="navbar-brand" href="#">Google Classroom</a>
+    <?php
+    if ($_SESSION["authenticated"] == "1") {
+    ?>
+      <ul class="nav navbar-nav ml-auto">
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <i class="fas fa-sign-out-alt" onclick="logout();"></i>
+          </a>
+        </li>
+      </ul>
+    <?php } ?>
   </nav>
