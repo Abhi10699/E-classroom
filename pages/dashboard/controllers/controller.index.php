96d878b746dc833481403197704061dc20bd830a
Abhi10699 <patelabhi10699@gmail.com>
2020-04-24 04:15:25 +0530

    fetches users classroom on login

--- pages/dashboard/controllers/controller.index.php
+++ pages/dashboard/controllers/controller.index.php
@@ -4,4 +4,13 @@ session_start();
   if($_SESSION["authenticated"] == "0"){
     header("Location: /pages/auth/signin.php");  
   }
+
+  // Fetch user using id stored in session
+
+  $myUser = User::FromId($_SESSION["USER_ID"])["user"];
+
+  // fetch classrooms as student
+  $s = Student::getAllClassrooms($myUser->getConnection(),$myUser->getUserId());  
+  
+  // fetch classrooms as a teacher
 ?>
