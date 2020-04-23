529c3598801484d686b99eae5e38f8c6bc75874d
Abhi10699 <patelabhi10699@gmail.com>
2020-04-24 04:20:36 +0530

    js logout function

---
+++ pages/dashboard/public/js/dashboard.index.js
@@ -0,0 +1,14 @@
+async function logout() {
+  const fd = new FormData();
+  fd.append("action","logout");
+  const res = await fetch("./controllers/controller.navbar.php", {
+    method:"post",
+    body:fd
+  });
+
+  const data = await res.json();
+
+  if(data.logout){
+    window.location="/";
+  }
+}
