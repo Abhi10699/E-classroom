335fb52f52fef62c7c6d5e673457858425231a0c
Abhi10699 <patelabhi10699@gmail.com>
2020-04-24 04:10:01 +0530

    added 2 helper functions

--- classes/database.php
+++ classes/database.php
@@ -10,9 +10,70 @@ function getDbConnection()
 
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-    
-  }
-  else{
+  } else {
     return $conn;
+  }
+}
+
+function PrepareFetchCall(mysqli $conn,string $query, array $params, callable $cb)
+{
+  // create query statement
+  $statement = $conn->prepare($query);
+
+  // check if parameters exists
+
+  if (isset($params)) {
+    // set parameters
+    $statement->bind_param($params["dTypes"], ...$params["params"]);
+  }
+
+
+  // execute query
+
+  $statement->execute();
+
+  // get result
+
+  $data = $statement->get_result();
+
+  if ($data->num_rows > 0) {
+    $result = array(
+      "error" => false,
+      "data" => $data,
+    );
+    // pass row into a callback function
+    return $cb($result);
+  } else {
+    $errResult = array(
+      "error" => true,
+      "message" => "No data found"
+    );
+
+    return $cb($errResult);
+  }
+}
+
+
+// for insert,update actions
+function PrepareUpdateCall(mysqli $conn,string $query, array $params)
+{
+  // create query prepare statement
+  $statement = $conn->prepare($query);
+
+  // check if parameters exists
+  if (isset($params)) {
+    // set parameters
+    $statement->bind_param($params["dTypes"], ...$params["params"]);
+  }
+
+  // execute query
+
+  $err = $statement->execute();
+
+
+  if ($err == false) {
+    return false;
+  } else {
+    return true;
   }
 }
