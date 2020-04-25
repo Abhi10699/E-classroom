4c9087bb86c781e51fc7ef7c2d1c177baa531fa6
Abhi10699 <patelabhi10699@gmail.com>
2020-04-25 18:35:12 +0530

    fixed a query

--- sql/queries.insert.sql
+++ sql/queries.insert.sql
@@ -4,7 +4,7 @@ insert into Classroom (FK_admin_id,classroom_name,description) values(1,"Java fr
 
 -- Add participants with id 2 in Classroom 
 
-insert into Participants (FK_class_id,FK_user_id) values(2,1);
+insert into Participants (FK_class_id,FK_user_id) values(2,4);
 
 -- Update classroom admin
 
