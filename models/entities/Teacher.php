cc66b4bd19622b5926a863feaeb9003e93bbc73e
Abhi10699 <patelabhi10699@gmail.com>
2020-04-25 18:29:24 +0530

    added function to fetch classes a user is taking

--- models/entities/Teacher.php
+++ models/entities/Teacher.php
@@ -1,9 +1,32 @@
 <?php
-  // load user base class
-  require_once "./User.php";
 
-  class Teacher{
-    
+// require_once($_SERVER["DOCUMENT_ROOT"] . "/classes/database.php");
+class Teacher
+{
+
+  public static function getClassroms(mysqli $dbcon, int $adminId)
+  {
+    $statment = "select Classroom.classroom_id,Classroom.classroom_name,Classroom.description from Classroom inner join Users on Users.id = Classroom.FK_admin_id where Users.id = ?";
+    $params = array(
+      "dTypes" => "i",
+      "params" => array($adminId)
+    );
+
+    return Database::PrepareFetchCall($dbcon, $statment, $params, function ($response) {
+
+      if ($response["error"] == true) {
+        return array(
+          "error" => true,
+          "message" => "You dont conduct any classrooms"
+        );
+      } else {
+        $classrooms = array();
+        while ($row = $response["data"]->fetch_assoc()) {
+          array_push($classrooms, $row);
+        }
+
+        return $classrooms;
+      }
+    });
   }
-
-?>
+}
