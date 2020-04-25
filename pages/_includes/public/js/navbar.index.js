b237d8bf491c1e30ef37c0ea52d3bd9f7df28ca1
Abhi10699 <patelabhi10699@gmail.com>
2020-04-25 18:32:47 +0530

    ajax function to create and join classroom

---
+++ pages/_includes/public/js/navbar.index.js
@@ -0,0 +1,54 @@
+// Navbar functions
+async function createClassroom() {
+  const classroomName = document.getElementById("classroom_name").value;
+  const description = document.getElementById("classroom_desc").value;
+  const fd = new FormData();
+
+  fd.append("classroom_name", classroomName);
+  fd.append("description", description);
+  fd.append("action", "createClassroom");
+
+  const res = await fetch(
+    "/pages/_includes/controllers/controller.navbar.php",
+    {
+      method: "post",
+      body: fd,
+    }
+  );
+
+  const data = await res.json();
+  // check for any errors
+  if (data.error) {
+    alert(data.error.message);
+    return;
+  } else {
+    window.location = "/pages/dashboard";
+  }
+}
+
+async function joinClassroom() {
+  const classroom_id = parseInt(document.getElementById("classroom_id").value);
+  const fd = new FormData();
+
+  fd.append("classroom_id", classroom_id);
+  fd.append("action", "joinClassroom");
+
+  const res = await fetch(
+    "/pages/_includes/controllers/controller.navbar.php",
+    {
+      method: "post",
+      body: fd,
+    }
+  );
+
+  const data = await res.json();
+  // check for any errors
+  if (data.error) {
+    alert(data.error.message);
+    return;
+  } else {
+    window.location = "/pages/dashboard";
+  }
+
+  // update the UI with new classrooms;
+}
