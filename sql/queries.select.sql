eae80001239807df6699293dafcaaf7d338c1bb7
Abhi10699 <patelabhi10699@gmail.com>
2020-04-25 18:35:41 +0530

    added a query to select a user from a classroom

--- sql/queries.select.sql
+++ sql/queries.select.sql
@@ -8,7 +8,7 @@ select * from Classroom where classroom_id = 1;
 
 -- Get all classrooms of a particular user
 
-select FK_class_id from Participants where FK_user_id;
+select FK_class_id from Participants where FK_user_id = 1;
 
 -- Select User from Participant table (something like that)
 
@@ -21,3 +21,5 @@ select Classroom.classroom_id,Users.username,Users.email,Classroom.classroom_nam
 -- Select all the classrooms of a student with a given id
 select Classroom.classroom_id,Classroom.classroom_name,Classroom.description from Classroom inner join Participants on Participants.FK_class_id=Classroom.classroom_id inner join Users on Users.id = Participants.FK_user_id where Participants.FK_user_id = ?;
 
+-- Select a student of id ? from a classroom ?
+select * from Participants where FK_user_id = 1 and FK_class_id = 1
