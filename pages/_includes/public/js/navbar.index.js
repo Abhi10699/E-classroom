1197d6c7317c3f681ba92f3438b5e99dcdfdd71d
Abhi10699 <patelabhi10699@gmail.com>
2020-04-28 19:20:53 +0530

    implemented a method to logout

--- pages/_includes/public/js/navbar.index.js
+++ pages/_includes/public/js/navbar.index.js
@@ -52,3 +52,18 @@ async function joinClassroom() {
 
   // update the UI with new classrooms;
 }
+
+async function logout() {
+  const fd = new FormData();
+  fd.append("action","logout");
+  const res = await fetch("./controllers/controller.navbar.php", {
+    method:"post",
+    body:fd
+  });
+
+  const data = await res.json();
+
+  if(data.logout){
+    window.location="/";
+  }
+}
