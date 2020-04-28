8f5d25ca8c848ea47a6ed9caec29812ba36fdc83
Abhi10699 <patelabhi10699@gmail.com>
2020-04-28 19:23:04 +0530

    removed logout function

--- pages/dashboard/public/js/dashboard.index.js
+++ pages/dashboard/public/js/dashboard.index.js
@@ -1,14 +1,14 @@
-async function logout() {
-  const fd = new FormData();
-  fd.append("action","logout");
-  const res = await fetch("./controllers/controller.navbar.php", {
-    method:"post",
-    body:fd
-  });
+// async function logout() {
+//   const fd = new FormData();
+//   fd.append("action","logout");
+//   const res = await fetch("../controllers/controller.navbar.php", {
+//     method:"post",
+//     body:fd
+//   });
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  if(data.logout){
-    window.location="/";
-  }
-}
+//   if(data.logout){
+//     window.location="/";
+//   }
+// }
